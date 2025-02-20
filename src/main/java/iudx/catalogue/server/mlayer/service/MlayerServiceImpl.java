aed66f98f42ad871fa042166da1362c16a6d509e
Tanvi Prasad <tanvi.prasad@datakaveri.org>
2025-02-20 08:57:03 +0530

    query fixed to delete non null values

--- src/main/java/iudx/catalogue/server/mlayer/service/MlayerServiceImpl.java
+++ src/main/java/iudx/catalogue/server/mlayer/service/MlayerServiceImpl.java
@@ -415,7 +415,7 @@ public class MlayerServiceImpl implements MlayerService {
   public Future<JsonObject> getMlayerPopularDatasets(String instance) {
     Promise<JsonObject> promise = Promise.promise();
     String query = GET_HIGH_COUNT_DATASET.replace("$1", databaseTable);
-    LOGGER.debug("postgres query" + query);
+    LOGGER.debug("postgres query " + query);
     postgresService.executeQuery(query).onComplete(dbHandler -> {
       if (dbHandler.succeeded()) {
         JsonArray popularDataset = dbHandler.result().getJsonArray("results");
@@ -436,7 +436,7 @@ public class MlayerServiceImpl implements MlayerService {
         mlayerPopularDatasets.getMlayerPopularDatasets(instance, popularRgs)
             .onComplete(getPopularDatasetsHandler -> {
               if (getPopularDatasetsHandler.succeeded()) {
-                LOGGER.info("Success: Getting data for the landing page.");
+                LOGGER.debug("Success: Getting data for the landing page.");
                 promise.complete(getPopularDatasetsHandler.result());
               } else {
                 LOGGER.error("Fail: Getting data for the landing page.");
